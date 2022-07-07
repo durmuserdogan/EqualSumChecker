b40e25dd3d12f772311777a6f4c5346bc04b5691
durmuserdogan <drmserdogan@outlook.com>
2022-07-07 10:57:20 +0300

    first

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int first, int second, int third){
+        int sum = first+second;
+        if (sum==third){
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean t =EqualSumChecker.hasEqualSum(1,1,1);
+        System.out.println(t);
+    }
+}
